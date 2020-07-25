63add62e84bfe4ee25a82e2dac51c2d18b7d8110
Raghu Bhairi <raghu.b@Turvo-raghu.b>
2020-07-25 11:58:54 +0530

    Added loginButton element

--- e2e/pages/login-page.ts
+++ e2e/pages/login-page.ts
@@ -4,6 +4,7 @@ import { ElementFinder, element, by } from "protractor";
 export class LoginPage {
     private usernameEle: ElementFinder = element(by.id('username'));
     private passwordEle: ElementFinder = element(by.id('password'));
+    private loginButtonEle: ElementFinder = element(by.buttonText('Sign in'));
     private loggedInMsgTextEle: ElementFinder = element(by.css('p[ng-if="correctMessage"]'));
 
 
@@ -15,6 +16,9 @@ export class LoginPage {
         return this.passwordEle.sendKeys(password);
     }
 
+    public clickOnLoginButton() {
+        return this.loginButtonEle.click();
+    }
     public getLogggedInMessageText() {
         return this.loggedInMsgTextEle.getText();
     }
